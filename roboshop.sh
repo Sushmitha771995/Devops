d371d5cd3fc3fa8071ac64aec79875df4d4e3d41
sushmita <sushmita771995@gmail.com>
2020-11-05 22:43:39 +0530

    roboshop first commit

--- roboshop.sh
+++ roboshop.sh
@@ -4,5 +4,6 @@
 echo -e "\e[1;4;35msetting up $1\e[0m"
 echo "installing nginx"
 yum install nginx -y >> /dev/null
+echo $?
 #echo -e "\e[1;4;35msentting up catalogue\e[0m"
 #echo -e "\e[1;4;35msetting up cart\e[0m"
