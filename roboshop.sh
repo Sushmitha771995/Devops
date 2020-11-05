8d433eb08ad5e28175ff3fbf50e28bbc52a54dc7
sushmita <sushmita771995@gmail.com>
2020-11-05 23:43:03 +0530

    function

--- roboshop.sh
+++ roboshop.sh
@@ -5,13 +5,17 @@ service=$1
 case $service in
 frontend)
 echo -e "\e[1;4;35msetting up $1\e[0m"
+echo "installing nginx"
+yum install nginx -y >> /dev/null
 ;;
 catalogue)
 echo -e "\e[1;4;35msentting up $1\e[0m"
+echo "installing nodejs"
+;;
+cart)
+echo -e "\e[1;4;35msetting up cart\e[0m"
+echo 'installing mongodb'
 ;;
 esac
 
-#echo "installing nginx"
-#yum install nginx -y >> /dev/null
-#echo -e "\e[1;4;35msentting up catalogue\e[0m"
-#echo -e "\e[1;4;35msetting up cart\e[0m"
+
