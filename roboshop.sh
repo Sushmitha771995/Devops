5651a13fdc0587f839d44c66f80432b22981bff5
sushmita <sushmita771995@gmail.com>
2020-11-05 20:16:27 +0530

    roboshop first commit

--- roboshop.sh
+++ roboshop.sh
@@ -1,5 +1,5 @@
 #!bin/bash
 #This is coding for the roboshop
-echo -e "\e[4;35msetting up frontend\e[0m"
-echo -e "\e[4;34msentting up catalogue\e[0m"
-echo -e "\e[4;32msetting up cart\e[0m"
+echo -e "\e[1;4;35msetting up frontend\e[0m"
+echo -e "\e[1;4;35msentting up catalogue\e[0m"
+echo -e "\e[1;4;35msetting up cart\e[0m"
