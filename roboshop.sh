ac20797a80074ba81d8444ba59ec9364bb5deab2
sushmita <sushmita771995@gmail.com>
2020-11-05 23:36:56 +0530

    function

--- roboshop.sh
+++ roboshop.sh
@@ -1,10 +1,17 @@
 #!bin/bash
 #This is coding for the roboshop
 
+service=$1
+case $1 in
+frontend)
 echo -e "\e[1;4;35msetting up $1\e[0m"
-echo "installing nginx"
-yum install nginx -y >> /dev/null
-exit 100
-echo $?
+;;
+case $1 in
+catalogue)
+echo -e "\e[1;4;35msentting up $1\e[0m"
+
+
+#echo "installing nginx"
+#yum install nginx -y >> /dev/null
 #echo -e "\e[1;4;35msentting up catalogue\e[0m"
 #echo -e "\e[1;4;35msetting up cart\e[0m"
