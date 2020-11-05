e3a6fb8c54657eba33177c40bdda3f5ba333f284
sushmita <sushmita771995@gmail.com>
2020-11-05 23:39:43 +0530

    function

--- roboshop.sh
+++ roboshop.sh
@@ -1,12 +1,11 @@
-#!bin/bash
+!bin/bash
 #This is coding for the roboshop
 
 service=$1
-case $1 in
+case $service in
 frontend)
 echo -e "\e[1;4;35msetting up $1\e[0m"
 ;;
-case $1 in
 catalogue)
 echo -e "\e[1;4;35msentting up $1\e[0m"
 ;;
