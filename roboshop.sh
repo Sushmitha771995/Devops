ae768470a97df3d337fe1cbd51d1af24c67db1db
sushmita <sushmita771995@gmail.com>
2020-11-05 20:17:15 +0530

    roboshop first commit

--- roboshop.sh
+++ roboshop.sh
@@ -1,5 +1,6 @@
 #!bin/bash
 #This is coding for the roboshop
 echo -e "\e[1;4;35msetting up frontend\e[0m"
+yum install nginx -y
 echo -e "\e[1;4;35msentting up catalogue\e[0m"
 echo -e "\e[1;4;35msetting up cart\e[0m"
