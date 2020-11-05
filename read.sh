0d7190880dd5053822b8132a9115e000a1d8ca61
sushmita <sushmita771995@gmail.com>
2020-11-05 19:56:25 +0530

    colour check

--- read.sh
+++ read.sh
@@ -3,6 +3,7 @@ echo "it is more user friendly"
 echo -e  "tell me what is the syntax"
 echo -e "line1\nline2"
 
-echo -e "\e[41mhey!colour check[0m"
-echo -e "\e[35mso what is magata let me [36mtryyyyyy[0m"
+echo -e "\e[41mhey!colour check\e[0m"
+echo -e "\e[35mso what is magata let me \e[36mtryyyyyy\e[0
+m"
 
