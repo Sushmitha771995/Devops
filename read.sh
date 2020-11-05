72692e861cb6744977d41cbc97344625c7105ea8
sushmita <sushmita771995@gmail.com>
2020-11-05 22:04:44 +0530

    roboshop first commit

--- read.sh
+++ read.sh
@@ -4,6 +4,9 @@ echo -e  "tell me what is the syntax"
 echo -e "line1\nline2"
 
 echo -e "\e[41mhey!colour check\e[0m"
-echo -e "\e[35mso what is magata let me \e[36mtryyyyyy\e[0
-m"
+echo -e "\e[35mso what is magata let me \e[36mtryyyyyy\e[0m"
+
+#promptig input from the user using read command
+read -p "enter your name "  name
+echo your name is $name
 
--- roboshop.sh
+++ roboshop.sh
@@ -1,6 +1,7 @@
 #!bin/bash
 #This is coding for the roboshop
 echo -e "\e[1;4;35msetting up frontend\e[0m"
+echo "installing nginx"
 yum install nginx -y
 echo -e "\e[1;4;35msentting up catalogue\e[0m"
 echo -e "\e[1;4;35msetting up cart\e[0m"
