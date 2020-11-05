76b7e36823df79fff6a1318d5d65728eca6cf101
sushmita <sushmita771995@gmail.com>
2020-11-06 00:03:11 +0530

    function

--- roboshop.sh
+++ roboshop.sh
@@ -2,18 +2,43 @@
 #This is coding for the roboshop
 
 service=$1
+installation=$2
+
+systtemctl()
+{
+  systemctl enable nginx
+  systemctl start nginx
+  systemctl enable nginx
+}
+heading()
+{
+ echo -e "\e[1;4;35msetting up $1\e[0m"
+}
 case $service in
 frontend)
-echo -e "\e[1;4;35msetting up $1\e[0m"
+ heading
+#echo -e "\e[1;4;35msetting up $1\e[0m"
 echo "installing nginx"
 yum install nginx -y >> /dev/null
+systemctl enable nginx >>/dev/null
+systemctl start nginx
+curl -s -L -o /tmp/frontend.zip "https://dev.azure.com/DevOps-Batches/f4b641c1-99db-46d1-8110-5c6c24ce2fb9/_apis/git/repositories/a781da9c-8fca-4605-8928-53c962282b74/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
+cd /usr/share/nginx/html
+rm -rf *
+unzip /tmp/frontend.zip
+mv static/* .
+rm -rf static README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+systemctl restart nginx
 ;;
 catalogue)
-echo -e "\e[1;4;35msentting up $1\e[0m"
+  heading
+#echo -e "\e[1;4;35msentting up $1\e[0m"
 echo "installing nodejs"
 ;;
 cart)
-echo -e "\e[1;4;35msetting up cart\e[0m"
+  heading
+#echo -e "\e[1;4;35msetting up cart\e[0m"
 echo 'installing mongodb'
 ;;
 *)
