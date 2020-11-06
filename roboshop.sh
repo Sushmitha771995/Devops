6965136a1a30cfa6143f950725e0fb6116a0b732
sushmita <sushmita771995@gmail.com>
2020-11-06 10:54:55 +0530

    function

--- roboshop.sh
+++ roboshop.sh
@@ -28,6 +28,7 @@ frontend)
 echo -n "installing nginx"
 yum install nginx -y &>>$LOG_FILE
 status_check $?
+echo -n "Downloading frontendk"
 curl -s -L -o /tmp/frontend.zip "https://dev.azure.com/DevOps-Batches/f4b641c1-99db-46d1-8110-5c6c24ce2fb9/_apis/git/repositories/a781da9c-8fca-4605-8928-53c962282b74/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true" >>$LOG_FILE
 status_check $?
 cd /usr/share/nginx/html
