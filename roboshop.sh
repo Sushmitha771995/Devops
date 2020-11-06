fb747d67b8cf3ce4efc7919ebb154517f9505640
sushmita <sushmita771995@gmail.com>
2020-11-06 11:35:17 +0530

    function

--- roboshop.sh
+++ roboshop.sh
@@ -25,17 +25,17 @@ esac
 heading
 case $service in
 frontend)
-echo -n "installing nginx"
+echo -n "installing nginx\t"
 yum install nginx -y &>>$LOG_FILE
 status_check $?
-echo -n "Downloading frontend "
+echo -n "Downloading frontend \t"
 curl -s -L -o /tmp/frontend.zip "https://dev.azure.com/DevOps-Batches/f4b641c1-99db-46d1-8110-5c6c24ce2fb9/_apis/git/repositories/a781da9c-8fca-4605-8928-53c962282b74/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true" >>$LOG_FILE
 status_check $?
-echo "Clearing old docs"
+echo -n "Clearing old docs\t"
 cd /usr/share/nginx/html
 rm -rf *
 status_check $?
-echo -n "extacting frontend"
+echo -n "extacting frontend\t"
 unzip /tmp/frontend.zip &>>$LOG_FILE
 status_check $?
 
