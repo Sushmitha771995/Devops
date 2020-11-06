db06e8625a852892bb0865e5aa99bce2d96800a2
sushmita <sushmita771995@gmail.com>
2020-11-06 11:32:30 +0530

    function

--- roboshop.sh
+++ roboshop.sh
@@ -28,16 +28,29 @@ frontend)
 echo -n "installing nginx"
 yum install nginx -y &>>$LOG_FILE
 status_check $?
-echo -n "Downloading frontendk"
+echo -n "Downloading frontend "
 curl -s -L -o /tmp/frontend.zip "https://dev.azure.com/DevOps-Batches/f4b641c1-99db-46d1-8110-5c6c24ce2fb9/_apis/git/repositories/a781da9c-8fca-4605-8928-53c962282b74/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true" >>$LOG_FILE
 status_check $?
+echo "Clearing old docs"
 cd /usr/share/nginx/html
-#rm -rf *
-#unzip /tmp/frontend.zip
-#mv static/* .
-#rm -rf static README.md
-#mv localhost.conf /etc/nginx/default.d/roboshop.conf
-#systemctl restart nginx
+rm -rf *
+status_check $?
+echo "extacting frontend"
+unzip /tmp/frontend.zip &>>$LOG_FILE
+status_check $?
+
+mv static/* .
+rm -rf static README.md
+
+echo "Update ngnx configuration"
+
+
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+status_check $?
+
+systemctl enable nginx &>>$LOG_FILE
+systemctl restart nginx &>>$LOG_FILE
+
 ;;
 catalogue)
   heading
