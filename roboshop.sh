0532445040516fc7b9f40b95456bb1887aeb94be
sushmita <sushmita771995@gmail.com>
2020-11-06 10:36:39 +0530

    function

--- roboshop.sh
+++ roboshop.sh
@@ -4,22 +4,26 @@
 service=$1
 installation=$2
 
-systtemctl()
-{
-  systemctl enable nginx
-  systemctl start nginx
-  systemctl enable nginx
-}
-heading($1)
+
+heading()
 {
  echo -e "\e[1;4;35msetting up $1\e[0m"
 }
+
+heading
 case $service in
 frontend)
- heading
-#echo -e "\e[1;4;35msetting up $1\e[0m"
 echo "installing nginx"
-yum install nginx -y >> /dev/null
+yum install nginx -y &>>$LOG_FILE
+case $? in
+0)
+  echo -e "\e[32mSUCCESS\e[0m"
+  ;;
+*)
+  echo -e "\e[31mFAILURE\e[0m"
+    ;;
+esac
+
 systemctl enable nginx >>/dev/null
 systemctl start nginx
 curl -s -L -o /tmp/frontend.zip "https://dev.azure.com/DevOps-Batches/f4b641c1-99db-46d1-8110-5c6c24ce2fb9/_apis/git/repositories/a781da9c-8fca-4605-8928-53c962282b74/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
