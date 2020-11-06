0c5d7e8988a8a227090e7a023c56a11315312ecb
sushmita <sushmita771995@gmail.com>
2020-11-06 10:38:54 +0530

    function

--- roboshop.sh
+++ roboshop.sh
@@ -14,7 +14,7 @@ heading
 case $service in
 frontend)
 echo "installing nginx"
-yum install nginx -y &>>$LOG_FILE
+yum install nginx -y &>>logfile
 case $? in
 0)
   echo -e "\e[32mSUCCESS\e[0m"
@@ -24,8 +24,8 @@ case $? in
     ;;
 esac
 
-systemctl enable nginx >>/dev/null
-systemctl start nginx
+#systemctl enable nginx >>/dev/null
+#systemctl start nginx
 curl -s -L -o /tmp/frontend.zip "https://dev.azure.com/DevOps-Batches/f4b641c1-99db-46d1-8110-5c6c24ce2fb9/_apis/git/repositories/a781da9c-8fca-4605-8928-53c962282b74/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
 cd /usr/share/nginx/html
 rm -rf *
