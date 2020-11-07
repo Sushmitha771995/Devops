ef46bc1e2d01db372d7709e51fb6325c07025fc7
sushmita <sushmita771995@gmail.com>
2020-11-07 13:07:14 +0530

    function

--- roboshop.sh
+++ roboshop.sh
@@ -25,20 +25,20 @@ heading
 
 case $service in
 frontend)
-echo -n "installing nginx\t\t"
+echo -n "installing nginx    "
 yum install nginx -y &>>$LOG_FILE
 status_check $?
 
-echo -n "Downloading frontend \t\t"
+echo -n "Downloading frontend  "
 curl -s -L -o /tmp/frontend.zip "https://dev.azure.com/DevOps-Batches/f4b641c1-99db-46d1-8110-5c6c24ce2fb9/_apis/git/repositories/a781da9c-8fca-4605-8928-53c962282b74/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true" >>$LOG_FILE
 status_check $?
 
-echo -n "Clearing old docs\t"
+echo -n "Clearing old docs  "
 cd /usr/share/nginx/html
 rm -rf *
 status_check $?
 
-echo -n "extacting frontend\t\t"
+echo -n "extacting frontend   "
 unzip /tmp/frontend.zip &>>$LOG_FILE
 status_check $?
 
@@ -54,7 +54,7 @@ systemctl restart nginx &>>$LOG_FILE
 ;;
 catalogue)
 heading
-echo -n "installing nodejs\t\t"
+echo -n "installing nodejs     "
 echo '[mongodb-org-4.2]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
