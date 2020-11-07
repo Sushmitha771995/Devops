c05eb36a315dad239ac2750b41716e6d88a7d576
sushmita <sushmita771995@gmail.com>
2020-11-07 13:56:22 +0530

    function

--- roboshop.sh
+++ roboshop.sh
@@ -70,7 +70,7 @@ yum install -y mongodb-org &>>$LOG_FILE
 status_check $?
 
 echo -n "changing IP address "
-sed -i -e "s/127.0.0.1/0.0.0.0" /etc/mongod.conf &>>LOG_FILE
+sed -i -e "s/127.0.0.1/0.0.0.0/" /etc/mongod.conf &>>LOG_FILE
 status_check $?
 
 echo -n "extracting schema"
