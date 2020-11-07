3f75dcf0781a940cf5f636949d3472eaa78caff1
sushmita <sushmita771995@gmail.com>
2020-11-07 23:25:26 +0530

    yes

--- roboshop.sh
+++ roboshop.sh
@@ -111,15 +111,15 @@ catalogue)
     ;;
     esac
 
-    echo -n "downloading configuration files"
+  echo -n "downloading configuration files"
   curl -s -L -o /tmp/catalogue.zip "https://dev.azure.com/DevOps-Batches/f4b641c1-99db-46d1-8110-5c6c24ce2fb9/_apis/git/repositories/1a7bd015-d982-487f-9904-1aa01c825db4/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true" &>>$Downloading
  status_check $?
 
 echo -n "extracting catalogue file"
  cd /home/roboshop
- mkdir catalogue
+ mkdir -p catalogue
  cd catalogue
- unzip /tmp/catalogue.zip &>>$LOG_FILE
+ unzip -o /tmp/catalogue.zip &>>$LOG_FILE
  status_check $?
 
 echo -n "Download dependent packages"
