f395051e6c6b301b5c2fe32e6e720ac3c9c8b882
sushmita <sushmita771995@gmail.com>
2020-11-08 23:54:09 +0530

    yes

--- roboshop.sh
+++ roboshop.sh
@@ -333,7 +333,24 @@ ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';" >/tmp/reset.sql
     mysql -u root -ppassword <shipping.sql &>>$LOG_FILE
     status_check $?
     ;;
+rabbitmq)
+  echo -n " installing python package"
+  yum install https://packages.erlang-solutions.com/erlang/rpm/centos/7/x86_64/esl-erlang_22.2.1-1~centos~7_amd64.rpm -y
+echo -n "Setup YUM repositories for RabbitMQ"
+ curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
 
+echo -n "Install RabbitMQ"
+yum install rabbitmq-server -y
+
+echo -n "Start RabbitMQ"
+systemctl enable rabbitmq-server
+ systemctl start rabbitmq-server
+
+echo -n "Create application user"
+rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl set_user_tags roboshop administrator
+ rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+;;
 
 *)
 echo -n "not listed in service"
