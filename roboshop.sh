25b86851f68fe99d9a06619689ffcf278624fc88
sushmita <sushmita771995@gmail.com>
2020-11-08 17:17:25 +0530

    yes

--- roboshop.sh
+++ roboshop.sh
@@ -290,6 +290,30 @@ status_check $?
 systemctl enable shipping &>>$LOG_FILE
 ;;
 
+mysql)
+  echo -n "setig up repo"
+  echo '[mysql57-community]
+name=MySQL 5.7 Community Server
+baseurl=http://repo.mysql.com/yum/mysql-5.7-community/el/7/$basearch/
+enabled=1
+gpgcheck=0' > /etc/yum.repos.d/mysql.repo
+status_check &>>$LOG_FILE
+
+echp -n "remov maria and install sql"
+yum remove mariadb-libs -y  &>>$LOG_FILE
+yum install mysql-community-server -y &>>$LOG_FILE
+status_check $?
+
+systemctl enable mysql &>>$LOG_FILE
+status_check $?
+systemctl start mysql &>>$LOG_FILE
+status_check $?
+
+sleep 30
+
+;;
+
+
 
 *)
 echo -n "not listed in service"
