b0a2ebef3e4c0c9a926351033fac35088e3c717e
sushmita <sushmita771995@gmail.com>
2020-11-09 00:25:44 +0530

    yes

--- roboshop.sh
+++ roboshop.sh
@@ -391,7 +391,15 @@ status_check $?
 #Update the roboshop user and group id in payment.ini file.
 chown roboshop:roboshop /home/roboshop/payment -R
 
+
+
 mv /home/roboshop/payment/systemd.service /etc/systemd/system/payment.service &>>$LOG_FILE
+
+sed -i -e "s/CARTHOST/cart-test.firstdevops.tk/" /etc/systemd/system/cart.service &>>$LOG_FILE
+sed -i -e "s/USERHOST/user-test.firstdevops.tk/" /etc/systemd/system/cart.service &>>$LOG_FILE
+sed -i -e "s/AMQPHOST/rabbitmq-test.firstdevops.tk/" /etc/systemd/system/cart.service &>>$LOG_FILE
+status_check $?
+
 # systemctl daemon-reload
 # systemctl enable payment
 # systemctl start payment
