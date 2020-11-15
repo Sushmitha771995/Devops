e6e553cf34e90e8083109c9f3949fbb50560d9e8
sushmita <sushmita771995@gmail.com>
2020-11-15 12:07:31 +0530

    yes

--- roboshop.sh
+++ roboshop.sh
@@ -78,7 +78,7 @@ echo -e "\e[3232minstalling dependencies\e[0m"
 curl -s -L -o /tmp/catalogue.zip "https://dev.azure.com/DevOps-Batches/f4b641c1-99db-46d1-8110-5c6c24ce2fb9/_apis/git/repositories/1a7bd015-d982-487f-9904-1aa01c825db4/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true" &>>$LOG_FILE
 status_check $?
 cd /home/roboshop
-mkdir -po catalogue
+mkdir -p catalogue
 cd catalogue
 unzip /tmp/catalogue.zip &>>$LOG_FILE
 status_check $?
