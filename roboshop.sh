85b524dbd0b656a35697beecb6ad5704cf1e331c
sushmita <sushmita771995@gmail.com>
2020-11-17 21:38:11 +0530

    yes

--- roboshop.sh
+++ roboshop.sh
@@ -135,15 +135,31 @@ status_check $?
 
 
 catalogue)
-
 node_js "https://dev.azure.com/DevOps-Batches/f4b641c1-99db-46d1-8110-5c6c24ce2fb9/_apis/git/repositories/1a7bd015-d982-487f-9904-1aa01c825db4/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
-
 ;;
 
- user)
+user)
 node_js "https://dev.azure.com/DevOps-Batches/f4b641c1-99db-46d1-8110-5c6c24ce2fb9/_apis/git/repositories/360c1f78-e8ed-41e8-8b3d-bdd12dc8a6a1/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
 ;;
 
+redis)
+ echo -e "\e[3232mInstalling redis\e[0m"
+ yum install epel-release yum-utils -y &>>$LOG_FILE
+ yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>$LOG_FILE
+ yum-config-manager --enable remi &>>$LOG_FILE
+ yum install redis -y  &>>$LOG_FILE
+ status_check $?
+
+ echo -e "\e[3232mUpdate config files\e[0m"
+ sed -i -e "s/127.0.0.1/0.0.0.0/" /etc/redis.conf &>>$LOG_FILE
+ status_check $?
+
+ echo -e "\e[3232mStarting redis service\e[0m"
+systemctl enable redis &>>$LOG_FILE
+ status_check $?
+systemctl start redis &>>$LOG_FILE
+ status_check $?
+;;
 cart)
 node_install
 app_user
