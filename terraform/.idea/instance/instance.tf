2ed283c311a76e1a5ac9639240136828cdd85b26
sushmita <sushmita771995@gmail.com>
2020-12-06 14:01:12 +0530

    yes

--- terraform/.idea/instance/instance.tf
+++ terraform/.idea/instance/instance.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region  = "us-east-1"
-  aws_access_key_id = "AKIAWNXXXJECEBULJNP4"
-  aws_secret_access_key = "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"
+  access_key = "AKIAWNXXXJECEBULJNP4"
+  secret_key = "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"
 }
 resource "aws_instance" "myinstance" {
   ami = "ami-052ed3344670027b3"
