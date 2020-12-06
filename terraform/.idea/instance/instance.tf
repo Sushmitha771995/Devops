52c2f243dbf82c4dd0c44d1384935490c72d3d45
sushmita <sushmita771995@gmail.com>
2020-12-06 13:57:44 +0530

    yes

--- terraform/.idea/instance/instance.tf
+++ terraform/.idea/instance/instance.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  region  = us-east-1
+  aws_access_key_id = "AKIAWNXXXJECEBULJNP4"
+  aws_secret_access_key = "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"
+}
 resource "aws_instance" "myinstance" {
   ami = "ami-052ed3344670027b3"
   instance_type = "t2.micro"
