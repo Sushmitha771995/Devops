ff57016b78149584e2f4d287f82a041a65c77029
sushmita <sushmita771995@gmail.com>
2020-12-07 14:34:26 +0530

    yes

--- terraform/.idea/s3/s3.tf
+++ terraform/.idea/s3/s3.tf
@@ -1,6 +1,6 @@
   terraform {
     backend "s3" {
-      bucket = "my-bucket-675"
+      bucket = "my-bucket-6754"
       key    = "statesample/terraform.tf"
       region = "us-east-1"
     }
@@ -10,7 +10,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "myinstance" {
-  ami = "ami-052ed3344670027b3"
-  instance_type = "t2.micro"
-}
+#resource "aws_instance" "myinstance" {
+#  ami = "ami-052ed3344670027b3"
+ # instance_type = "t2.micro"
+#}
