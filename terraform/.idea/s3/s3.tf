cf105a8e7e4280304ac3aa4b4f434a462ade119f
sushmita <sushmita771995@gmail.com>
2020-12-07 15:00:44 +0530

    yes

--- terraform/.idea/s3/s3.tf
+++ terraform/.idea/s3/s3.tf
@@ -10,7 +10,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-#resource "aws_instance" "myinstance" {
-#  ami = "ami-052ed3344670027b3"
- # instance_type = "t2.micro"
-#}
+resource "aws_instance" "myinstance" {
+  ami = "ami-052ed3344670027b3"
+ instance_type = "t2.micro"
+}
